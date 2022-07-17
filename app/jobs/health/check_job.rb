8cde1efed253755bbfd5be0d2f6f2d4e518a66f7
n30n <mikekolich@gmail.com>
2022-07-16 20:18:28 -0400

    On Health::CheckJob.after_perform create Health::CheckRecord

--- app/jobs/health/check_job.rb
+++ app/jobs/health/check_job.rb
@@ -3,5 +3,15 @@ module Health
     def perform(health_check_class, health_check_method)
       health_check_class.new.send(health_check_method)
     end
+
+    after_perform do |job|
+      health_check_class = job.arguments.first
+      health_check_method = job.arguments.second
+
+      Health::CheckRecord.create!(
+        health_check_name: "#{health_check_class.to_s}##{health_check_method.to_s}",
+        ran_at: Time.zone.now
+      )
+    end
   end
 end
--- spec/health/jobs/check_job_spec.rb
+++ spec/health/jobs/check_job_spec.rb
@@ -1,12 +1,22 @@
 require "rails_helper"
 
-describe Health::CheckJob do
+describe Health::CheckJob, active_job: true, type: :job do
+  include ActiveJob::TestHelper
+
   let!(:mock_health_check) { MockHealthCheck }
   let!(:mock_instance) { mock_health_check.new }
 
-  describe "#perform" do
-    subject { described_class.perform_now(mock_health_check, :example) }
+  subject(:health_check_job) do
+    perform_enqueued_jobs do
+      described_class.perform_later(mock_health_check, :example)
+    end
+  end
 
+  before do
+    allow(Health::CheckRecord).to receive(:create!).and_return(true)
+  end
+
+  describe "#perform" do
     before do
       allow(mock_health_check).to receive(:new).and_return(mock_instance)
       allow(mock_instance).to receive(:example)
@@ -15,6 +25,17 @@ describe Health::CheckJob do
 
     it "instantiates a health_check_class and calls the health_check_method" do
       expect(mock_instance).to have_received(:example)
+    end
+  end
+
+  describe "#after_perform" do
+    before do
+      subject
+    end
+
+    it "creates a new HealthCheck::Record" do
+      expect(Health::CheckRecord).to have_received(:create!).
+          with(hash_including({ health_check_name: "MockHealthCheck#example"}))
     end
   end
 end
